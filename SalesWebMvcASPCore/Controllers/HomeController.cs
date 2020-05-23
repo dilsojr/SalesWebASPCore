eba10952fccb533d4a4e9d4b84042ca6342f16a2
dilsojr <51032855+dilsojr@users.noreply.github.com>
2020-05-23 17:45:09 -0300

    First Controller and Razor pages tests

--- SalesWebMvcASPCore/Controllers/HomeController.cs
+++ SalesWebMvcASPCore/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvcASPCore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Dilso Roecker Júnior";
+            ViewData["Email"] = "dilso22@gmail.com";
 
             return View();
         }
