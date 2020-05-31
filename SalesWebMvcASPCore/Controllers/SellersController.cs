b70e68ec5930bfe35d6803b280decc5d7f448184
dilsojr <51032855+dilsojr@users.noreply.github.com>
2020-05-31 19:14:32 -0300

    Update seller and custom service exception

--- SalesWebMvcASPCore/Controllers/SellersController.cs
+++ SalesWebMvcASPCore/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvcASPCore.Models;
 using SalesWebMvcASPCore.Models.ViewModels;
 using SalesWebMvcASPCore.Services;
+using SalesWebMvcASPCore.Services.Exceptions;
 
 namespace SalesWebMvcASPCore.Controllers
 {
@@ -75,5 +76,43 @@ namespace SalesWebMvcASPCore.Controllers
             return View(obj);
 
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var obj = _sellerService.FindByID(id.Value);
+            if (obj == null)
+                return NotFound();
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerformViewModel viewModel = new SellerformViewModel{ Departments = departments, Seller = obj };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+                return NotFound();
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
+
 }
--- SalesWebMvcASPCore/Services/SellerService.cs
+++ SalesWebMvcASPCore/Services/SellerService.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvcASPCore.Data;
 using SalesWebMvcASPCore.Models;
+using SalesWebMvcASPCore.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SalesWebMvcASPCore.Services
@@ -38,6 +40,22 @@ namespace SalesWebMvcASPCore.Services
             var obj = _context.Sellers.Find(id);
             _context.Sellers.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Sellers.Any(x => x.Id == obj.Id))
+                throw new NotFoundException("Id not found");
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
